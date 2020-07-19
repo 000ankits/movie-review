ea2b8978769919f6948f6218a9c61aa02c47e5cc
unknown <ankit.sircar@gmail.comgit config user.emailgit config --global user.name Ankit>
2020-07-19 08:54:20 +0530

    added login & register routes only, added results with OMDB API

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express'),
 	dotEnv = require('dotenv'),
 	bodyparser = require('body-parser'),
+	{ Client } = require('pg'),
+	session = require('express-session'),
+	flash = require('connect-flash'),
+	superAgent = require('superagent'),
 	methodOverride = require('method-override');
-app = express();
+
+const app = express();
 
 dotEnv.config({ path: './.env' });
 app.set('view engine', 'ejs');
@@ -14,13 +19,53 @@ app.use((req, res, next) => {
 	next();
 });
 
+const dbconn = new Client({
+	connectionString : process.env.DATABASE_URL,
+	ssl              : {
+		rejectUnauthorized : false
+	}
+});
+
 const port = process.env.PORT || 8888;
+
+dbconn.connect();
 
 // ==============================
 // Routes
 // ==============================
 app.get('/', (req, res) => {
-	res.render('home');
+	res.redirect('/searchmovie');
+});
+
+app.get('/searchmovie', (req, res) => {
+	res.render('search');
+});
+
+app.post('/searchmovie', (req, res) => {
+	const apiKey = process.env.apiKey;
+	superAgent
+		.post('http://www.omdbapi.com/?apikey=' + apiKey + '&s=' + req.body.name + '&type=' + req.body.type)
+		.then((result) => {
+			let data = JSON.parse(result.text);
+			return res.render('results', { results: data.Search });
+			// return res.send(data);
+		})
+		.catch((e) => {
+			console.log(e);
+			return res.send(e.message);
+		});
+});
+
+// ==============================
+// Auth Routes
+// ==============================
+
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+app.post('/register', (req, res) => {
+	res.send(req.body);
 });
 
 app.listen(port, () => {
